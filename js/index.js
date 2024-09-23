221398238d44f3b2efc58bb584b4dc3819a73e19
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-23 08:30:03 +0600

    all functionalities added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Donation and History button toggle
 const donationSection = document.getElementById('donation-section');
 const historySection = document.getElementById('history-section');
 const toggleHistoryBtn = document.getElementById('toggle-history-btn');
@@ -20,3 +21,61 @@ toggleDonationBtn.addEventListener('click',function(){
     
     
 })
+
+
+let accountBalance = document.getElementById('account-balance');
+let divForHistoryAdding = document.getElementById('add-history-div');
+let date = new Date();
+
+
+
+function getNumberFromString(text) {
+    const result = parseFloat(text);
+    return result;
+}
+
+
+function donateMoney(event) {
+    let donateHeading = event.target.parentElement.parentElement.children[1].innerText.slice(7);
+    
+    let fundedBalance = event.target.parentElement.parentElement.children[0].children[0].children[1];
+    let fundedNumberBalance = getNumberFromString(fundedBalance.innerText); 
+
+    let accountNumberBalance = getNumberFromString(accountBalance.innerText);
+
+    let donateInputAmount = event.target.parentElement.previousElementSibling;
+    let donateAmountNumber = getNumberFromString(donateInputAmount.value);
+
+    let modalBox = event.target.parentElement;
+    modalBox.setAttribute("onclick", "my_modal_5.showModal()");
+
+    if (donateAmountNumber > accountNumberBalance || donateAmountNumber <= 0 || isNaN(donateAmountNumber) ) {
+        modalBox.removeAttribute("onclick");
+        alert('Invalid input');
+       
+        return;
+    }
+
+ 
+    
+   
+    
+            
+
+    fundedBalance.innerText = fundedNumberBalance + donateAmountNumber;
+    accountBalance.innerText = accountNumberBalance - donateAmountNumber;
+    divForHistoryAdding.innerHTML += `
+            <div class="p-4 lg:p-8 border border-black1/[0.1] border-solid shadow-sm rounded-2xl">
+            <h1 class="font-bold text-base lg:text-xl text-black1 mb-4">${donateAmountNumber} Taka is Donated ${donateHeading}</h1>
+            <p class="font-light text-sm lg:text-base text-black1/[0.7]">Date : ${date}</p>
+          </div>
+    `;
+    
+    
+}
+
+
+
+
+
+
